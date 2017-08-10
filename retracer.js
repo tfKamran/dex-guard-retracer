3934799e6748368658695fdec265643ce3f10f43
Kamran Ahmed Ansari <team.radiant@gmail.com>
2017-08-10 14:28:12 +0530

    Interpret class names and their mappings from raw mappings

--- retracer.js
+++ retracer.js
@@ -1,9 +1,26 @@
 (function() {
+    function generateMap(rawMappings) {
+        const mappings = {};
+        const lines = rawMappings.split("\n");
+
+        for (var index = 0; index < lines.length - 1; index++) {
+            const line = lines[index];
+            const split = line.split(" -> ");
+
+            if (!line.startsWith("    ")) {
+                mappings[split[1].substring(0, split[1].length - 1)] = { "class": split[0] };
+            }
+        };
+
+        return mappings;
+    }
+
     function retrace(log, mapping) {
         return log;
     }
 
     module.exports = {
-        retrace
+        retrace,
+        generateMap
     }
 })();
--- test/retracer.js
+++ test/retracer.js
@@ -1,7 +1,22 @@
 const assert = require('assert');
 const Retracer = require('../retracer');
 
+const mappingFile1 = "com.example.test.TestClass -> o.wM:\n"
+    + "    java.lang.String TAG -> ॱ\n"
+    + "    android.content.Context mContext -> ˏ\n"
+    + "    android.net.Uri mContentUri -> ˎ\n";
+
 describe('Retracer', function() {
+    describe('generateMap', function() {
+        it('should return an object', function() {
+            assert.equal(typeof {}, typeof Retracer.generateMap(""));
+        });
+
+        it('should interpret class name when raw mappings are provided', function() {
+            assert.equal("com.example.test.TestClass", Retracer.generateMap(mappingFile1)["o.wM"].class);
+        });
+    });
+
     describe('retrace', function() {
         it('should return string', function() {
             assert.equal(typeof "", typeof Retracer.retrace(""));
