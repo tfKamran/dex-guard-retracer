b1807a43d369eaea391a272faf7e9eeafbc92d68
Kamran Ahmed Ansari <team.radiant@gmail.com>
2017-08-12 17:56:21 +0530

    Show help when input arguments are not provided

--- index.js
+++ index.js
@@ -3,6 +3,21 @@
 const fs = require('fs');
 const Retracer = require('./retracer');
 
+if (process.argv.length != 4) {
+    console.log(
+        "You need to pass in two arguments:\n"
+        + "\n"
+        + "1. Mapping file\n"
+        + "2. Log file\n"
+        + "\n"
+        + "Example:\n"
+        + "\n"
+        + "\tdex-guard-retracer ./test-mapping.txt ./test-log.txt\n"
+        );
+
+    return;
+}
+
 const rawMappings = fs.readFileSync(process.argv[2], 'utf-8');
 const logs = fs.readFileSync(process.argv[3], 'utf-8');
 
